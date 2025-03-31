cd4981a7345241883f26e368d83b5b93fdf6b104
Mel <ailmel.clemente@gmail.com>
2025-03-31 21:56:59 +0800

    Updated gitignore to remove dist

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ailmel04.github.io/", // GitHub Pages subpath
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "./index.html", // Ensure Vite modifies this correctly
-    },
-  },
+  base: '/ailmel04.github.io/', // Change this to match your repo name
 });
