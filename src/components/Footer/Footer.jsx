be62731b0c068996fd5ed538a3057a34e8eec092
Mel <ailmel.clemente@gmail.com>
2025-04-14 17:53:43 +0800

    footer updates to remove Subscribe

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,13 +11,13 @@ const Footer = () => {
                 <img src={footer_logo} alt="" />
                 <p>Innovative and detail-oriented UI/UX Engineer and Front-End Developer with over 16 years of experience </p>
             </div>
-            <div className="footer-top-right">
+            {/* <div className="footer-top-right">
                 <div className="footer-email-input">
                     <img src={user_icon} alt="" />
                     <input type="email" placeholder='Enter your email' />
                 </div>
                 <div className="footer-subscribe">Subscribe</div>
-            </div>
+            </div> */}
         </div>
         <hr />
         <div className="footer-bottom">
